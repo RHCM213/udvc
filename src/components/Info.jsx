c6445c7111b8ebc40044fd08339505b4b8bda268
Rafael M <rhernan10@gmail.com>
2022-04-30 20:10:07 +0100

    Lógica de clicar no artigo de Info e mostrar o modal respectivo implementada (NOTA: quando executado o CSS prop position: fixed, os dados apresentados ainda nao sao os correctos)

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Info.css"
+
 
 
 export default function Info({articles}) {
-  console.log( articles );
-  
+  //console.log( articles );
+  const [showArt, setShowArt] = useState(false);
+        
+  function toggleShow() {
+    setShowArt(!showArt);
+  }
+
 
   return (
-
-    
-  
     <div>
       {articles.map( (article, index) => (
 
-      <article key={index}>
-          <img src= { "/images/artg/" + article.img } alt={ article.desc }/>
-          <h2>{ article.title }</h2>
-          <p>{ article.text.split('\n').map(txt => <p>{txt}</p>) }</p>
-          <p>{ article.postdate }</p>
+      <article key={index} className={"article"}>
+        <img src= { "/images/artg/" + article.img } alt={ article.desc } onClick={toggleShow}/>
+        <h2>{ article.title }</h2>
+        {showArt && 
+          <div className={"modalart"}>
+            <article className={"singleart"}>
+              <button onClick={toggleShow}>close</button>
+              <img src= { "/images/artg/" + article.img } alt={ article.desc }/>
+              <h2>{ article.title }</h2>
+              <p>{ article.text.split('\n').map(txt => <p>{txt}</p>) }</p>
+              <p className={"postdate"}>{ article.postdate }</p>  
+            </article>
+          </div>
+        }
       </article> 
       ))
-      }
-      
-      
-     
+      } 
     </div>
-  
-
-
-
-
-
-
-
-
   
   )
 }
